b77f4597e5e6a101d5dedb9f377f37852a418f92
Ashley Plasket <ashley.plasket@gmail.com>
2023-06-09 11:48:45 -0400

    add alternatives based on feedback

--- app/controllers/ride_mechanics_controller.rb
+++ app/controllers/ride_mechanics_controller.rb
@@ -3,5 +3,17 @@ class RideMechanicsController < ApplicationController
     ride_mechanic = RideMechanic.create!(ride_id: params[:ride_id], mechanic_id: params[:id])
     mechanic = Mechanic.find_by(id: params[:id])
     redirect_to "/mechanics/#{mechanic.id}"
+
+    ## alternative:
+    #mechanic = Mechanic.find(params[:id])
+
+    #another alternative:
+    # ride_mechanic = RideMechanic.create!(ride_id: params[:ride_id], mechanic_id: params[:id])
+    # redirect_to "/mechanics/#{params[:id]}"
+
+    #or to be safe, even:
+    # mechanic = Mechanic.find(params[:id])
+    # ride_mechanic = RideMechanic.create!(ride_id: params[:ride_id], mechanic_id: mechanic.id)
+    # redirect_to "/mechanics/#{mechanic.id}"
   end
 end
